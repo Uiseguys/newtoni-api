51c246a6f2a4328aabed19f135ef2f314414396f
Cris Shaki <cmshaki@gmail.com>
2019-09-11 15:04:18 +0300

    Modification to host

--- index.js
+++ index.js
@@ -7,10 +7,18 @@ if (require.main === module) {
   const config = {
     rest: {
       port: +(process.env.PORT || 3000),
-      host: process.env.HOST || 'localhost',
+      host: process.env.HOST,
       openApiSpec: {
         // useful when used with OpenAPI-to-GraphQL to locate your application
         setServersFromRequest: true,
+      },
+      cors: {
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
+        maxAge: 86400,
+        credentials: true,
       },
     },
   };
