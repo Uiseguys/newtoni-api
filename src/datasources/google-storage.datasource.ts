1f107c330c98aa9351d5564ab34893c35c8416db
Cris Shaki <cmshaki@gmail.com>
2019-09-14 19:26:35 +0300

    Trying to use object instead of creating authentication file

--- src/datasources/google-storage.datasource.ts
+++ src/datasources/google-storage.datasource.ts
@@ -2,15 +2,29 @@ import {inject} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 import * as config from './google-storage.datasource.json';
 
+const newConfig = {
+  ...config,
+  type: process.env.GOOGLE_STORAGE_TYPE,
+  project_id: process.env.GOOGLE_STORAGE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_STORAGE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_STORAGE_PRIVATE_KEY,
+  client_email: process.env.GOOGLE_STORAGE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_STORAGE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_STORAGE_AUTH_URI,
+  token_uri: process.env.GOOGLE_STORAGE_TOKEN_URI,
+  auth_provider_x509_cert_url:
+    process.env.GOOGLE_STORAGE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_STORAGE_CLIENT_X509_CERT_URL,
+};
 export class GoogleStorageDataSource extends juggler.DataSource {
-	static dataSourceName = 'GoogleStorage';
+  static dataSourceName = 'GoogleStorage';
 
-	constructor(
-		@inject('datasources.config.GoogleStorage', {
-			optional: true,
-		})
-		dsConfig: object = config,
-	) {
-		super(dsConfig);
-	}
+  constructor(
+    @inject('datasources.newConfig.GoogleStorage', {
+      optional: true,
+    })
+    dsConfig: object = newConfig,
+  ) {
+    super(dsConfig);
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,20 @@ import * as fs from 'fs';
 export {NewtoniApiApplication};
 
 export async function main(options: ApplicationConfig = {}) {
-	const app = new NewtoniApiApplication(options);
+  const app = new NewtoniApiApplication(options);
 
-
-	/* Add this line, it add a service to the app after that you can
+  /* Add this line, it add a service to the app after that you can
 	call them in the controller with dependency injection, like:
 	@inject('services.StorageGCService') */
-	app.serviceProvider(GoogleStorageServiceProvider);
+  app.serviceProvider(GoogleStorageServiceProvider);
 
-	await app.boot();
-	await app.migrateSchema();
-	await app.start();
+  await app.boot();
+  await app.migrateSchema();
+  await app.start();
 
-	const url = app.restServer.url;
-	console.log(`Server is running at ${url}`);
-	console.log(`Try ${url}/ping`);
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
 
-	return app;
+  return app;
 }
