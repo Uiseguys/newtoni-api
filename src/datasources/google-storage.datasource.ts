588b0bdc88351a529caf5405d2c064929a726619
Cris Shaki <cmshaki@gmail.com>
2019-09-14 12:47:28 +0300

    Trying to create JSON object

--- src/datasources/google-storage.datasource.ts
+++ src/datasources/google-storage.datasource.ts
@@ -4,16 +4,16 @@ import * as config from './google-storage.datasource.json';
 
 const newConfig = {
 	...config,
-	type: process.env.GOOGLE_STORAGE_TYPE,
-	project_id: process.env.GOOGLE_STORAGE_PROJECT_ID,
-	private_key_id: process.env.GOOGLE_STORAGE_PRIVATE_KEY_ID,
-	private_key: process.env.GOOGLE_STORAGE_PRIVATE_KEY,
-	client_email: process.env.GOOGLE_STORAGE_CLIENT_EMAIL,
-	client_id: process.env.GOOGLE_STORAGE_CLIENT_ID,
-	auth_uri: process.env.GOOGLE_STORAGE_AUTH_URI,
-	token_uri: process.env.GOOGLE_STORAGE_TOKEN_URI,
-	auth_provider_x509_cert_url: process.env.GOOGLE_STORAGE_AUTH_PROVIDER_X509_CERT_URL,
-	x509_cert_url: process.env.GOOGLE_STORAGE_X509_CERT_URL
+	"type": process.env.GOOGLE_STORAGE_TYPE,
+	"project_id": process.env.GOOGLE_STORAGE_PROJECT_ID,
+	"private_key_id": process.env.GOOGLE_STORAGE_PRIVATE_KEY_ID,
+	"private_key": process.env.GOOGLE_STORAGE_PRIVATE_KEY,
+	"client_email": process.env.GOOGLE_STORAGE_CLIENT_EMAIL,
+	"client_id": process.env.GOOGLE_STORAGE_CLIENT_ID,
+	"auth_uri": process.env.GOOGLE_STORAGE_AUTH_URI,
+	"token_uri": process.env.GOOGLE_STORAGE_TOKEN_URI,
+	"auth_provider_x509_cert_url": process.env.GOOGLE_STORAGE_AUTH_PROVIDER_X509_CERT_URL,
+	"x509_cert_url": process.env.GOOGLE_STORAGE_X509_CERT_URL
 };
 
 export class GoogleStorageDataSource extends juggler.DataSource {
