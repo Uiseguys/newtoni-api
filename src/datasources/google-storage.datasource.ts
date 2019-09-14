dfd5830e3433ae48f95206b5ec6009f4dbdb8879
Cris Shaki <cmshaki@gmail.com>
2019-09-14 19:37:47 +0300

    Trying to use object instead of creating authentication file

--- src/datasources/google-storage.datasource.ts
+++ src/datasources/google-storage.datasource.ts
@@ -1,26 +1,31 @@
 import {inject} from '@loopback/core';
 import {juggler} from '@loopback/repository';
-import * as config from './google-storage.datasource.json';
+//import * as config from './google-storage.datasource.json';
 
-const newConfig = {
-  ...config,
-  type: process.env.GOOGLE_STORAGE_TYPE,
-  project_id: process.env.GOOGLE_STORAGE_PROJECT_ID,
-  private_key_id: process.env.GOOGLE_STORAGE_PRIVATE_KEY_ID,
-  private_key: process.env.GOOGLE_STORAGE_PRIVATE_KEY,
-  client_email: process.env.GOOGLE_STORAGE_CLIENT_EMAIL,
-  client_id: process.env.GOOGLE_STORAGE_CLIENT_ID,
-  auth_uri: process.env.GOOGLE_STORAGE_AUTH_URI,
-  token_uri: process.env.GOOGLE_STORAGE_TOKEN_URI,
-  auth_provider_x509_cert_url:
-    process.env.GOOGLE_STORAGE_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.GOOGLE_STORAGE_CLIENT_X509_CERT_URL,
-};
+let newConfig = `{
+  "name": "GoogleStorage",
+  "connector": "loopback-component-storage",
+  "provider": "google",
+  "projectId": "weingut-schneckenhof",
+  "nameConflict": "makeUnique",
+"type": "${process.env.GOOGLE_STORAGE_TYPE}",
+"project_id": "${process.env.GOOGLE_STORAGE_PROJECT_ID}",
+"private_key_id": "${process.env.GOOGLE_STORAGE_PRIVATE_KEY_ID}",
+"private_key": "${process.env.GOOGLE_STORAGE_PRIVATE_KEY}",
+"client_email": "${process.env.GOOGLE_STORAGE_CLIENT_EMAIL}",
+"client_id": "${process.env.GOOGLE_STORAGE_CLIENT_ID}",
+"auth_uri": "${process.env.GOOGLE_STORAGE_AUTH_URI}",
+"token_uri": "${process.env.GOOGLE_STORAGE_TOKEN_URI}",
+"auth_provider_x509_cert_url": "${process.env.GOOGLE_STORAGE_AUTH_PROVIDER_X509_CERT_URL}",
+"client_x509_cert_url": "${process.env.GOOGLE_STORAGE_CLIENT_X509_CERT_URL}"
+}`;
+newConfig = JSON.parse(newConfig);
+
 export class GoogleStorageDataSource extends juggler.DataSource {
   static dataSourceName = 'GoogleStorage';
 
   constructor(
-    @inject('datasources.newConfig.GoogleStorage', {
+    @inject('datasources.config.GoogleStorage', {
       optional: true,
     })
     dsConfig: object = newConfig,
