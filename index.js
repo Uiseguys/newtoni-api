3ef4de3c6fbd1cc68e7aa171ed4155c8c56de2a3
Cris Shaki <cmshaki@gmail.com>
2019-09-16 22:13:23 +0300

    removed json creating code

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ const application = require('./dist');
 const fs = require('fs');
 
 // Create google folder to contain authentication key if it doesn't exist
-if (!fs.existsSync('./keys/google')) {
-  fs.mkdirSync('./keys/google', {recursive: true});
-  console.log('created google folder');
+//if (!fs.existsSync('./keys/google')) {
+//fs.mkdirSync('./keys/google', {recursive: true});
+//console.log('created google folder');
 
-  /* Create Google Cloud Storage Key from environment Variables */
-  let googleStorageKey = process.env.GOOGLE_STORAGE_SERVICE_KEY;
+//[> Create Google Cloud Storage Key from environment Variables <]
+//let googleStorageKey = process.env.GOOGLE_STORAGE_SERVICE_KEY;
 
-  googleStorageKey = JSON.parse(googleStorageKey);
-  googleStorageKey = JSON.stringify(googleStorageKey);
+//googleStorageKey = JSON.parse(googleStorageKey);
+//googleStorageKey = JSON.stringify(googleStorageKey);
 
-  fs.writeFile('./keys/google/auth.json', googleStorageKey, 'utf-8', err => {
-    if (err) {
-      return console.log(err);
-    }
-    return console.log('Key File has been created successfully');
-  });
-}
+//fs.writeFile('./keys/google/auth.json', googleStorageKey, 'utf-8', err => {
+//if (err) {
+//return console.log(err);
+//}
+//return console.log('Key File has been created successfully');
+//});
+//}
 
 module.exports = application;
 
