e8b74e1a82768688fa88c19a704e80f1cd9e11cb
Cris Shaki <cmshaki@gmail.com>
2020-06-14 23:27:22 +0300

    Added netlify hook endpoint

--- src/controllers/setting.controller.ts
+++ src/controllers/setting.controller.ts
@@ -13,7 +13,6 @@ import {
   getModelSchemaRef,
   getWhereSchemaFor,
   patch,
-  put,
   del,
   requestBody,
 } from '@loopback/rest';
@@ -49,58 +48,32 @@ export class SettingController {
       },
     })
     settings: Setting,
-    @param.query.object('where', getWhereSchemaFor(Setting))
-    where?: Where<Setting>,
   ): Promise<Setting | Count> {
+    const where = {key: settings.key};
+    const allSettings = await this.settingRepository.find({});
     const findkey = await this.settingRepository.find({
-      where: {key: settings.key},
+      where: where,
     });
     if (findkey.length > 0) {
-      return this.settingRepository.updateAll(settings, {key: settings.key});
+      return this.settingRepository.updateAll(settings, where);
     }
-    settings.id = Math.floor(1000 + Math.random() * 9000);
+    if (allSettings) {
+      const maxId = allSettings.reduce((acc, value) => {
+        if (value) return acc >= value['id'] ? acc : value['id'];
+        return acc;
+      }, 0);
+      settings['id'] = maxId;
+    } else {
+      settings['id'] = 1;
+    }
     return this.settingRepository.create(settings);
-  }
-
-  @post('/settings/netlify', {
-    responses: {
-      '200': {
-        description: 'Settings model instance',
-        content: {'application/json': {schema: getModelSchemaRef(Setting)}},
-      },
-    },
-  })
-  async netlifyHook(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: {type: 'object'},
-        },
-      },
-    })
-    reqBody: object,
-  ): Promise<Count | Setting> {
-    const findNetlify = await this.settingRepository.find({
-      where: {key: 'netlifyHook'},
-    });
-    if (findNetlify.length > 0) {
-      return this.settingRepository.updateAll(
-        {value: reqBody},
-        {key: 'netlifyHook'},
-      );
-    }
-    return this.settingRepository.create({
-      id: Math.floor(1000 + Math.random() * 9000),
-      key: 'netlifyHook',
-      value: reqBody,
-    } as Setting);
   }
 
   @authenticate('BasicStrategy')
   @get('/settings/count', {
     responses: {
       '200': {
-        description: 'Settings model count',
+        description: 'Setting model count',
         content: {'application/json': {schema: CountSchema}},
       },
     },
@@ -116,7 +89,7 @@ export class SettingController {
   @get('/settings', {
     responses: {
       '200': {
-        description: 'Array of Settings model instances',
+        description: 'Array of Setting model instances',
         content: {
           'application/json': {
             schema: {type: 'array', items: getModelSchemaRef(Setting)},
@@ -136,7 +109,7 @@ export class SettingController {
   @patch('/settings', {
     responses: {
       '200': {
-        description: 'Settings PATCH success count',
+        description: 'Setting PATCH success count',
         content: {'application/json': {schema: CountSchema}},
       },
     },
@@ -157,14 +130,60 @@ export class SettingController {
   }
 
   @authenticate('BasicStrategy')
+  @get('/settings/{id}', {
+    responses: {
+      '200': {
+        description: 'Setting model instance',
+        content: {'application/json': {schema: getModelSchemaRef(Setting)}},
+      },
+    },
+  })
+  async findById(@param.path.number('id') id: number): Promise<Setting> {
+    return this.settingRepository.findById(id);
+  }
+
+  @authenticate('BasicStrategy')
   @del('/settings/{id}', {
     responses: {
       '204': {
-        description: 'Settings DELETE success',
+        description: 'Setting DELETE success',
       },
     },
   })
   async deleteById(@param.path.number('id') id: number): Promise<void> {
     await this.settingRepository.deleteById(id);
+  }
+
+  // NETLIFY <--> INCOMING WEBHOOKS HANDLING
+  @authenticate('BasicStrategy')
+  @post('/settings/netlify/deploy', {
+    responses: {
+      '200': {
+        description: 'Settings model instance',
+        content: {'application/json': {schema: {type: 'object'}}},
+      },
+    },
+  })
+  async deployResponse(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: {type: 'object'},
+        },
+      },
+    })
+    buildHook: object,
+  ): Promise<object> {
+    const where = {key: 'netlifyHook'};
+    const findKey = await this.settingRepository.find({where: where});
+    if (findKey.length > 0) {
+      return this.settingRepository.updateAll({value: buildHook}, where);
+    }
+    const id = Math.floor(1000 + Math.random() * 9000);
+    return this.settingRepository.create({
+      id: id,
+      key: 'netlifyHook',
+      value: buildHook,
+    });
   }
 }
